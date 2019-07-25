e9077947bc4a6c6d3d4d9c5ad9df66a73ead940c
Sérgio Marcelino <sergiofilhow@gmail.com>
2019-07-25 13:02:00 -0300

    adding some responses objects

--- src/main/java/org/entando/web/response/EntandoEntity.java
+++ src/main/java/org/entando/web/response/EntandoEntity.java
@@ -1,32 +1,10 @@
 package org.entando.web.response;
 
-import lombok.Getter;
-import lombok.Setter;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static java.util.Optional.ofNullable;
-
-@Getter@Setter
-public class EntandoEntity<T> extends RestResponse<T, Map<String, Object>> {
-
-    public EntandoEntity(final T payload) {
-        super(payload);
-    }
-
-    public EntandoEntity() {
-        super();
-    }
-
-    public void addMetadata(final String key, final Object value) {
-        ofNullable(getMetadata()).orElseGet(this::init)
-                .put(key, value);
-    }
-
-    private Map<String, Object> init() {
-        setMetadata(new HashMap<>());
-        return getMetadata();
-    }
+/**
+ * @deprecated use {@link SimpleRestResponse} instead
+ * @param <T>
+ */
+@Deprecated
+public class EntandoEntity<T> extends SimpleRestResponse<T> {
 
 }
---
+++ src/main/java/org/entando/web/response/SimpleRestResponse.java
@@ -0,0 +1,32 @@
+package org.entando.web.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Optional.ofNullable;
+
+@Getter@Setter
+public class SimpleRestResponse<T> extends RestResponse<T, Map<String, Object>> {
+
+    public SimpleRestResponse(final T payload) {
+        super(payload);
+    }
+
+    public SimpleRestResponse() {
+        super();
+    }
+
+    public void addMetadata(final String key, final Object value) {
+        ofNullable(getMetadata()).orElseGet(this::init)
+                .put(key, value);
+    }
+
+    private Map<String, Object> init() {
+        setMetadata(new HashMap<>());
+        return getMetadata();
+    }
+
+}
