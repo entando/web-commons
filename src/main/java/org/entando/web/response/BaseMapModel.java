31ad9b7edb271df1a2aa9bbf7dc7c1cc776bf9b5
Filipe Leandro <filipeleandro87@gmail.com>
2019-09-20 11:32:08 -0300

    Created base model class to use as a generic map response deserialization.

---
+++ src/main/java/org/entando/web/response/BaseMapModel.java
@@ -0,0 +1,29 @@
+package org.entando.web.response;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class BaseMapModel {
+    @JsonAnySetter
+    protected Map<String,Object> data = new HashMap<>();
+
+    @JsonAnyGetter
+    public Map<String,Object> getData() {
+        return data;
+    }
+
+    public BaseMapModel(Map<String,Object> map) {
+        data = Optional.ofNullable(map)
+            .orElse(new HashMap<>());
+    }
+
+    public void putAll(Map<String,Object> map) {
+        data.putAll(map);
+    }
+}
