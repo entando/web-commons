58f66457ed98a055976956a31fa3c0b2efb7d0dd
Kerruba <luca.cherubin@gmail.com>
2019-10-02 10:40:07 +0200

    Adds support to de-serialization of entando response with metaData instead of metadata

--- src/main/java/org/entando/web/response/RestResponse.java
+++ src/main/java/org/entando/web/response/RestResponse.java
@@ -8,6 +8,8 @@ import org.springframework.hateoas.ResourceSupport;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonSetter;
+
 @Getter@Setter
 @NoArgsConstructor
 public class RestResponse<T, M> extends ResourceSupport {
@@ -16,13 +18,23 @@ public class RestResponse<T, M> extends ResourceSupport {
     private M metadata;
     private List<RestError> errors = new ArrayList<>();
 
+    @JsonSetter("metaData")
+    public void setMetaData(M metadata) {
+        this.metadata = metadata;
+    }
+
+    @JsonSetter("metadata")
+    public void setMetadata(M metadata) {
+        this.metadata = metadata;
+    }
+
     public RestResponse(final T payload) {
         setPayload(payload);
     }
 
     public RestResponse(final T payload, final M metadata) {
         setPayload(payload);
-        setMetadata(metadata);
+        setMetaData(metadata);
     }
 
     public RestResponse(final T payload, final M metadata, final List<RestError> errors) {
