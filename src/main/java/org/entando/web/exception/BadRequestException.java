fd0dfa737c1f5526b7749062d9058f45a3b8cdc8
Filipe Leandro <filipeleandro87@gmail.com>
2019-12-05 12:03:30 -0300

    PDA-47 added convenience constructors

--- src/main/java/org/entando/web/exception/BadRequestException.java
+++ src/main/java/org/entando/web/exception/BadRequestException.java
@@ -2,8 +2,20 @@ package org.entando.web.exception;
 
 public class BadRequestException extends IllegalArgumentException {
 
+    public BadRequestException(String message, Throwable e) {
+        super(message, e);
+    }
+
+    public BadRequestException(Throwable e) {
+        this("org.entando.error.badFormatRequest", e);
+    }
+
     public BadRequestException() {
-        super("org.entando.error.badFormatRequest");
+        this((Throwable) null);
+    }
+
+    public BadRequestException(String message) {
+        this(message, null);
     }
 
 }
